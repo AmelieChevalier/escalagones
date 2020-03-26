009e8167d22343a2ef24a36eb84afc53c40ad52f
Amélie <ameliechevalier3006@gmail.com>
2020-03-26 19:44:30 +0100

    add the date in js for the event page

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.btn-navigation').click(function(){
         $(this).find('.barre').toggleClass('white');
         $('.nav-bar-escalade').toggleClass('isOpen');
 
     }); 
 });
+ */
+// Open alert when submit form
 
 function congrats(){
     alert("Merci pour votre message ! Nous reviendrons vers vous au plus vite !")
 };
+
+
+// add the date of the day on the page with events
+
+const d = new Date();
+
+const date = document.body.querySelector(".date");
+
+date.textContent = `${d.toLocaleDateString()}`;
